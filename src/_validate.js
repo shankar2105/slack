29a10b65a94a97fb4402c519d2df29bd7fad8475
Shankar <shankar21mail@gmail.com>
2018-12-03 18:03:38 +0530

    feat/optional_params: added support to optional params
    Search.all api now support for optional params

--- src/_validate.js
+++ src/_validate.js
@@ -4,7 +4,12 @@ module.exports = function validate(method, params) {
   // get all the requried params for this method
   let required = api[method]
   // collect any missing params
-  let missing = required.filter(param=> typeof params[param] === 'undefined')
+  let missing = required.filter(param=> {
+    if (param.indexOf('-opt-') === 0) {
+      return false;
+    }
+    return typeof params[param] === 'undefined'
+  })
   // optimistic: assume the best but have a plan for the worst
   return missing.length? Error(`${method} missing params: ${missing.join(', ')}`) : false
 }
